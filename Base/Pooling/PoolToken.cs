15fe125c19747a34ad994a9938c181de2d609e9e
Arseni <arseni.natapov@gmail.com>
2025-01-26 01:01:15 +0300

    Update PoolToken.cs

--- Base/Pooling/PoolToken.cs
+++ Base/Pooling/PoolToken.cs
@@ -1,26 +1,29 @@
 ﻿using System;
 
-public sealed class PoolToken : PooledObject<PoolToken>
+namespace BB
 {
-	object _object;
-	IPool _pool;
-	Action<object> _dispose;
-	public static PoolToken Get(object obj, IPool pool, Action<object> dispose)
+	public sealed class PoolToken : PooledObject<PoolToken>
 	{
+		object _object;
+		IPool _pool;
+		Action<object> _dispose;
+		public static PoolToken Get(object obj, IPool pool, Action<object> dispose)
+		{
 
-		var result = GetPooled();
+			var result = GetPooled();
 
-		result._object = obj;
-		result._pool = pool;
-		result._dispose = dispose;
+			result._object = obj;
+			result._pool = pool;
+			result._dispose = dispose;
 
-		return result;
-	}
-	public override void Dispose()
-	{
-		_dispose?.Invoke(_object);
-		_pool.ReturnToPool(_object);
+			return result;
+		}
+		public override void Dispose()
+		{
+			_dispose?.Invoke(_object);
+			_pool.ReturnToPool(_object);
 
-		base.Dispose();
+			base.Dispose();
+		}
 	}
 }
