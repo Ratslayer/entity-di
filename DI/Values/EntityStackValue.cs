c200951ce3767872c63b7e5d6be9826aa7e9612c
Arseni <arseni.natapov@gmail.com>
2025-04-17 03:17:36 +0300

    Added PushValueOnSpawn base class

--- DI/Values/EntityStackValue.cs
+++ DI/Values/EntityStackValue.cs
@@ -7,4 +7,11 @@
 		public static implicit operator bool(EntityStackValue<TSelf> e)
 			=> e.Value;
 	}
+
+	public abstract record BoolStackValue<TSelf> : StackValue<TSelf, bool>
+		where TSelf : BoolStackValue<TSelf>
+	{
+		public static implicit operator bool(BoolStackValue<TSelf> e)
+			=> e.Value;
+	}
 }
---
+++ DI/Values/PushValueOnSpawn.cs
@@ -0,0 +1,15 @@
+﻿using BB.Di;
+
+namespace BB
+{
+	public abstract record PushValueOnSpawn<TStack, TValue>(
+		TStack Stack,
+		TValue Value)
+		where TStack : StackValue<TStack, TValue>
+	{
+		[OnSpawn]
+		void OnSpawn() => Stack.Push(Value);
+		[OnDespawn]
+		void OnDespawn() => Stack.Remove(Value);
+	}
+}
