e45b328d96f6015a2fd48a0a20464f8740a43706
Arseni <arseni.natapov@gmail.com>
2025-07-19 18:01:36 +0300

    Added FlagStackValue

---
+++ DI/Values/FlagStackValue.cs
@@ -0,0 +1,9 @@
+﻿using BB.Di;
+using System;
+
+public abstract record FlagStackValue<TSelf, TEnum> : StackValue<TSelf, TEnum>
+	where TSelf : FlagStackValue<TSelf, TEnum>
+	where TEnum : Enum
+{
+	public bool HasFlag(TEnum e) => Value.HasFlag(e);
+}
