e9be45227ff005fda53315483c764beb537ecb13
Arseni <arseni.natapov@gmail.com>
2025-08-15 06:59:37 +0300

    Fixed pooled objects being disposed multiple times.

--- Base/Pooling/ProtectedPooledObject.cs
+++ Base/Pooling/ProtectedPooledObject.cs
@@ -9,14 +9,17 @@ namespace BB
 		protected ProtectedPooledObject() { }
 		protected static TSelf GetPooledInternal()
 		{
+
 			var result = _pool.Count == 0 ? new TSelf() : _pool.RemoveLast();
 			result.Counter = PooledDisposableUtils.GetNextCounter();
 			return result;
 		}
 		public virtual void Dispose()
 		{
-			_pool.Add((TSelf)this);
+			if (Counter == 0)
+				return;
 			Counter = 0;
+			_pool.Add((TSelf)this);
 		}
 		public DisposableToken GetToken() => new(this);
 		public DisposableToken<TSelf> GetTypedToken() => new((TSelf)this, Counter);
