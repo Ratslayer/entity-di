0de0d9a55a2823a7cccd6e9a208780060f2aa935
Arseni <arseni.natapov@gmail.com>
2025-09-20 15:19:57 +0300

    Added empty string validator

--- Base/Validation/AssignedObjectValidator.cs
+++ Base/Validation/AssignedObjectValidator.cs
@@ -1,27 +1,45 @@
 ﻿namespace BB
 {
-	public sealed class AssignedObjectValidator : ProtectedPooledObject<AssignedObjectValidator>,
-		IValidator
-	{
-		UnityEngine.Object _object;
-		string _propertyName;
-		public static AssignedObjectValidator GetPooled(UnityEngine.Object obj, string propertyName)
-		{
-			var result = GetPooledInternal();
-			result._object = obj;
-			result._propertyName = propertyName;
-			return result;
-		}
-
-		public bool IsValid(out string message)
-		{
-			if (_object)
-			{
-				message = "";
-				return true;
-			}
-			message = $"{_propertyName} has not been assigned";
-			return false;
-		}
-	}
+    public abstract class PooledValidator<TSelf, TValue> : ProtectedPooledObject<TSelf>, IValidator
+        where TSelf : PooledValidator<TSelf, TValue>, new()
+    {
+        protected TValue _object;
+        protected string _propertyName;
+        public static TSelf GetPooled(TValue obj, string propertyName)
+        {
+            var result = GetPooledInternal();
+            result._object = obj;
+            result._propertyName = propertyName;
+            return result;
+        }
+        public abstract bool IsValid(out string message);
+    }
+    public sealed class AssignedObjectValidator
+        : PooledValidator<AssignedObjectValidator, UnityEngine.Object>
+    {
+        public override bool IsValid(out string message)
+        {
+            if (_object)
+            {
+                message = "";
+                return true;
+            }
+            message = $"{_propertyName} has not been assigned";
+            return false;
+        }
+    }
+    public sealed class NotEmptyStringValidator
+        : PooledValidator<NotEmptyStringValidator, string>
+    {
+        public override bool IsValid(out string message)
+        {
+            if (!string.IsNullOrWhiteSpace(_object))
+            {
+                message = "";
+                return true;
+            }
+            message = $"{_propertyName} is null or whitespace";
+            return false;
+        }
+    }
 }
--- Base/Validation/Validator.cs
+++ Base/Validation/Validator.cs
@@ -3,67 +3,72 @@ using System.Runtime.CompilerServices;
 
 namespace BB
 {
-	public sealed class Validator : ProtectedPooledObject<Validator>
-	{
-		readonly List<IValidator> _validators = new();
-		Entity _entity;
-		BaseScriptableObject _asset;
-		string _fileName;
-		public static Validator GetPooled([CallerFilePath] string fileName = "")
-		{
-			var result = GetPooledInternal();
-			result._fileName = fileName;
-			return result;
-		}
-		public override void Dispose()
-		{
-			_entity = default;
-			_validators.DisposeAndClear();
-			base.Dispose();
-		}
-		public bool ValidateAndDispose()
-		{
-			var sb = PooledStringBuilder.GetPooled();
-			var result = true;
-			foreach (var validator in _validators)
-			{
-				var isValid = validator.IsValid(out var message);
-				if (isValid)
-					continue;
-				result = false;
-				if (!string.IsNullOrWhiteSpace(message))
-				{
-					if (sb.Empty)
-					{
-						if (_entity)
-							sb.AppendLine($"Validation error in Entity {_entity}");
-						else if (_asset)
-							sb.AppendLine($"Validation error in Asset {_asset.name}");
-						else sb.AppendLine($"Validation error");
-						sb.AppendLine($"File {_fileName}:");
-					}
-					sb.AppendLine(message);
-				}
-			}
-			if (!result)
-				Log.Error(sb.ToString());
-			sb.Dispose();
-			return result;
-		}
-		public Validator WithEntity(Entity entity)
-		{
-			_entity = entity;
-			return this;
-		}
-		public Validator WithAsset(BaseScriptableObject asset)
-		{
-			_asset = asset;
-			return this;
-		}
-		public Validator IsAssigned(UnityEngine.Object obj, string propertyName)
-		{
-			_validators.Add(AssignedObjectValidator.GetPooled(obj, propertyName));
-			return this;
-		}
-	}
+    public sealed class Validator : ProtectedPooledObject<Validator>
+    {
+        readonly List<IValidator> _validators = new();
+        Entity _entity;
+        BaseScriptableObject _asset;
+        string _fileName;
+        public static Validator GetPooled([CallerFilePath] string fileName = "")
+        {
+            var result = GetPooledInternal();
+            result._fileName = fileName;
+            return result;
+        }
+        public override void Dispose()
+        {
+            _entity = default;
+            _validators.DisposeAndClear();
+            base.Dispose();
+        }
+        public bool ValidateAndDispose()
+        {
+            var sb = PooledStringBuilder.GetPooled();
+            var result = true;
+            foreach (var validator in _validators)
+            {
+                var isValid = validator.IsValid(out var message);
+                if (isValid)
+                    continue;
+                result = false;
+                if (!string.IsNullOrWhiteSpace(message))
+                {
+                    if (sb.Empty)
+                    {
+                        if (_entity)
+                            sb.AppendLine($"Validation error in Entity {_entity}");
+                        else if (_asset)
+                            sb.AppendLine($"Validation error in Asset {_asset.name}");
+                        else sb.AppendLine($"Validation error");
+                        sb.AppendLine($"File {_fileName}:");
+                    }
+                    sb.AppendLine(message);
+                }
+            }
+            if (!result)
+                Log.Error(sb.ToString());
+            sb.Dispose();
+            return result;
+        }
+        public Validator WithEntity(Entity entity)
+        {
+            _entity = entity;
+            return this;
+        }
+        public Validator WithAsset(BaseScriptableObject asset)
+        {
+            _asset = asset;
+            return this;
+        }
+        public Validator IsAssigned(UnityEngine.Object obj, string propertyName)
+        {
+            _validators.Add(AssignedObjectValidator.GetPooled(obj, propertyName));
+            return this;
+        }
+        public Validator NotEmpty(string value, string propertyName)
+        {
+            _validators.Add(NotEmptyStringValidator.GetPooled(value, propertyName));
+            return this;
+        }
+    }
 }
