bf5485fd360e823c17fe36d77f0d5c8a7b9342b6
Arseni <arseni.natapov@gmail.com>
2025-09-27 10:57:51 +0300

    Added LessThan

--- Base/Utils/MathUtils.cs
+++ Base/Utils/MathUtils.cs
@@ -1,58 +1,62 @@
 ﻿using System;
 public static class MathUtils
 {
-	const float _epsilon = 0.00001f;
-	public static bool Approximately(this float a, float b) => Math.Abs(a - b) <= _epsilon;
-	public static bool Approximately(this double a, double b) => Math.Abs(a - b) <= double.Epsilon;
-	public static float Clamp(this float f, float min, float max)
-		=> Math.Clamp(f, min, max);
-	public static double Clamp(this double d, double min, double max)
-		=> Math.Clamp(d, min, max);
-	public static int Clamp(this int n, int min, int max)
-		=> Math.Clamp(n, min, max);
-	public static double Clamp01(this double d)
-		=> d.Clamp(0, 1);
-	public static float Clamp01(this float f)
-		=> f.Clamp(0, 1);
-	public static double Min0(this double d) => Math.Max(d, 0);
-	public static int Min0(this int d) => Math.Max(d, 0);
-	public static double Min(this double d, double v) => Math.Max(d, v);
-	public static double Max0(this double d) => Math.Min(d, 0);
-	public static double Max(this double d, double v) => Math.Min(d, v);
+    const float _epsilon = 0.00001f;
+    public static bool Approximately(this float a, float b) => Math.Abs(a - b) <= _epsilon;
+    public static bool Approximately(this double a, double b) => Math.Abs(a - b) <= double.Epsilon;
+    public static float Clamp(this float f, float min, float max)
+        => Math.Clamp(f, min, max);
+    public static double Clamp(this double d, double min, double max)
+        => Math.Clamp(d, min, max);
+    public static int Clamp(this int n, int min, int max)
+        => Math.Clamp(n, min, max);
+    public static double Clamp01(this double d)
+        => d.Clamp(0, 1);
+    public static float Clamp01(this float f)
+        => f.Clamp(0, 1);
+    public static double Min0(this double d) => Math.Max(d, 0);
+    public static int Min0(this int d) => Math.Max(d, 0);
+    public static double Min(this double d, double v) => Math.Max(d, v);
+    public static double Max0(this double d) => Math.Min(d, 0);
+    public static double Max(this double d, double v) => Math.Min(d, v);
 
-	public static double MapToRange(this double d, double min, double max)
-		=> min + (max - min) * d;
-	public static double ClampToRange(this double d, double min, double max)
-		=> d.Clamp01().MapToRange(min, max);
-	public static float MapToRange(this float f, float min, float max)
-		=> min + (max - min) * f;
-	public static float ClampToRange(this float f, float min, float max)
-		=> f.Clamp01().MapToRange(min, max);
-	public static bool IsBetween(this double d, double min, double max)
-		=> d >= min && d <= max;
-	public static bool IsZero(this float value) => value.Approximately(0);
-	public static bool IsZero(this double value) => value.Approximately(0);
-	public static bool NotZero(this double value) => !value.IsZero();
-	public static bool IsValid(this double value) => value is not double.NaN;
-	public static bool LessThanOrEquals(this float v1, float v2)
-		=> v1 <= v2 + _epsilon;
-	public static bool LessThanOrEquals(this double v1, double v2)
-		=> v1 <= v2 + _epsilon;
-	public static bool GreaterThanOrEquals(this float v1, float v2)
-		=> v1 >= v2 - _epsilon;
-	public static bool GreaterThanOrEquals(this double v1, double v2)
-		=> v1 >= v2 - _epsilon;
-	public static bool GreaterThan(this float v1, float v2)
-		=> v1 >= v2 + _epsilon;
-	public static bool GreaterThan(this double v1, double v2)
-		=> v1 >= v2 + _epsilon;
+    public static double MapToRange(this double d, double min, double max)
+        => min + (max - min) * d;
+    public static double ClampToRange(this double d, double min, double max)
+        => d.Clamp01().MapToRange(min, max);
+    public static float MapToRange(this float f, float min, float max)
+        => min + (max - min) * f;
+    public static float ClampToRange(this float f, float min, float max)
+        => f.Clamp01().MapToRange(min, max);
+    public static bool IsBetween(this double d, double min, double max)
+        => d >= min && d <= max;
+    public static bool IsZero(this float value) => value.Approximately(0);
+    public static bool IsZero(this double value) => value.Approximately(0);
+    public static bool NotZero(this double value) => !value.IsZero();
+    public static bool IsValid(this double value) => value is not double.NaN;
+    public static bool LessThanOrEquals(this float v1, float v2)
+        => v1 <= v2 + _epsilon;
+    public static bool LessThanOrEquals(this double v1, double v2)
+        => v1 <= v2 + _epsilon;
+    public static bool LessThan(this float v1, float v2)
+        => v1 <= v2 - _epsilon;
+    public static bool LessThan(this double v1, double v2)
+        => v1 <= v2 - _epsilon;
+    public static bool GreaterThanOrEquals(this float v1, float v2)
+        => v1 >= v2 - _epsilon;
+    public static bool GreaterThanOrEquals(this double v1, double v2)
+        => v1 >= v2 - _epsilon;
+    public static bool GreaterThan(this float v1, float v2)
+        => v1 >= v2 + _epsilon;
+    public static bool GreaterThan(this double v1, double v2)
+        => v1 >= v2 + _epsilon;
 
-	public static float SignOrZero(float value, float threshold)
-		=> Math.Abs(value) < threshold ? 0f : Math.Sign(value);
-	public static float Min0(this float value) => Math.Max(value, 0);
-	public static bool IsPositive(this double value)
-		=> value.IsValid() && value >= _epsilon;
+    public static float SignOrZero(float value, float threshold)
+        => Math.Abs(value) < threshold ? 0f : Math.Sign(value);
+    public static float Min0(this float value) => Math.Max(value, 0);
+    public static bool IsPositive(this double value)
+        => value.IsValid() && value >= _epsilon;
 
-	public static bool IsOdd(this int n) => n % 2 == 1;
-	public static bool IsEven(this int n) => n % 2 == 0;
+    public static bool IsOdd(this int n) => n % 2 == 1;
+    public static bool IsEven(this int n) => n % 2 == 0;
 }
--- DI/Install/EntityExtensions.cs
+++ DI/Install/EntityExtensions.cs
@@ -2,162 +2,166 @@
 using Cysharp.Threading.Tasks;
 using System.Threading;
 using System.Collections.Generic;
-using System.Collections;
 using UnityEngine;
 namespace BB
 {
-	public static class EntityExtensions
-	{
-		public static T Require<T>(this Entity entity)
-		{
-			if (entity._ref is null)
-				throw new DiException($"Trying to get component from a null entity");
-			return entity._ref.Require<T>();
-		}
-		public static T Get<T>(this Entity entity)
-		{
-			entity.Has(out T instance);
-			return instance;
-		}
-		public static async UniTask WaitForEvent<T>(this Entity entity, CancellationToken ct)
-		{
-			if (!entity.Has(out IEvent<T> e))
-				return;
-			await e.WaitForEvent(ct);
-		}
-		public static void Despawn(this Entity entity)
-		{
-			if (entity)
-				entity._ref.State = EntityState.Despawned;
-		}
-		public static void DespawnAndClear<T>(this ICollection<T> collection)
-			where T:Component
-		{
-			foreach (var element in collection)
-				element.Despawn();
-			collection.Clear();
-		}
+    public static class EntityExtensions
+    {
+        public static T Require<T>(this Entity entity)
+        {
+            if (entity._ref is null)
+                throw new DiException($"Trying to get component from a null entity");
+            return entity._ref.Require<T>();
+        }
+        public static void Require<T1, T2>(this Entity entity, out T1 t1, out T2 t2)
+        {
+            t1 = entity.Require<T1>();
+            t2 = entity.Require<T2>();
+        }
+        public static T Get<T>(this Entity entity)
+        {
+            entity.Has(out T instance);
+            return instance;
+        }
+        public static async UniTask WaitForEvent<T>(this Entity entity, CancellationToken ct)
+        {
+            if (!entity.Has(out IEvent<T> e))
+                return;
+            await e.WaitForEvent(ct);
+        }
+        public static void Despawn(this Entity entity)
+        {
+            if (entity)
+                entity._ref.State = EntityState.Despawned;
+        }
+        public static void DespawnAndClear<T>(this ICollection<T> collection)
+            where T : Component
+        {
+            foreach (var element in collection)
+                element.Despawn();
+            collection.Clear();
+        }
 
-		public static void Publish<T>(this Entity entity, T msg = default)
-		{
-			if (entity.Has(out IEvent<T> publisher))
-				publisher.Publish(msg);
-		}
-		public static void Publish<T>(this IEntity entity, T msg = default)
-		{
-			if (entity.Has(out IEvent<T> publisher))
-				publisher.Publish(msg);
-		}
-		
+        public static void Publish<T>(this Entity entity, T msg = default)
+        {
+            if (entity.Has(out IEvent<T> publisher))
+                publisher.Publish(msg);
+        }
+        public static void Publish<T>(this IEntity entity, T msg = default)
+        {
+            if (entity.Has(out IEvent<T> publisher))
+                publisher.Publish(msg);
+        }
 
-		public static void Get<VarType>(this Entity entity, ref VarType var)
-			where VarType : class
-		{
-			if (var is null)
-				entity.Has(out var);
-		}
-		public static bool Has<VarType>(this Entity entity, out VarType var)
-		{
-			if (!entity)
-			{
-				var = default;
-				return false;
-			}
-			return entity._ref.Has(out var);
-		}
-		public static bool Has<T1, T2>(this Entity entity, out T1 var1, out T2 var2)
-		{
-			if (!entity)
-			{
-				var1 = default;
-				var2 = default;
-				return false;
-			}
-			var result = entity.Has(out var1);
-			result &= entity.Has(out var2);
-			return result;
-		}
-		public static bool Has<T1, T2, T3>(this Entity entity, out T1 var1, out T2 var2, out T3 var3)
-		{
-			if (!entity)
-			{
-				var1 = default;
-				var2 = default;
-				var3 = default;
-				return false;
-			}
-			var result = entity.Has(out var1);
-			result &= entity.Has(out var2);
-			result &= entity.Has(out var3);
-			return result;
-		}
-		public static bool Has<T1, T2, T3, T4>(
-			this Entity entity,
-			out T1 var1, out T2 var2, out T3 var3, out T4 var4)
-		{
-			if (!entity)
-			{
-				var1 = default;
-				var2 = default;
-				var3 = default;
-				var4 = default;
-				return false;
-			}
-			var result = entity.Has(out var1);
-			result &= entity.Has(out var2);
-			result &= entity.Has(out var3);
-			result &= entity.Has(out var4);
-			return result;
-		}
-		public static bool Has<T1, T2, T3, T4, T5>(
-			this Entity entity,
-			out T1 var1, out T2 var2, out T3 var3, out T4 var4, out T5 var5)
-		{
-			if (!entity)
-			{
-				var1 = default;
-				var2 = default;
-				var3 = default;
-				var4 = default;
-				var5 = default;
-				return false;
-			}
-			var result = entity.Has(out var1);
-			result &= entity.Has(out var2);
-			result &= entity.Has(out var3);
-			result &= entity.Has(out var4);
-			result &= entity.Has(out var5);
-			return result;
-		}
 
-		public static bool Has<T1, T2, T3, T4, T5,T6>(
-			this Entity entity,
-			out T1 var1, out T2 var2, out T3 var3, out T4 var4, out T5 var5, out T6 var6)
-		{
-			if (!entity)
-			{
-				var1 = default;
-				var2 = default;
-				var3 = default;
-				var4 = default;
-				var5 = default;
-				var6 = default;
-				return false;
-			}
-			var result = entity.Has(out var1);
-			result &= entity.Has(out var2);
-			result &= entity.Has(out var3);
-			result &= entity.Has(out var4);
-			result &= entity.Has(out var5);
-			result &= entity.Has(out var6);
-			return result;
-		}
+        public static void Get<VarType>(this Entity entity, ref VarType var)
+            where VarType : class
+        {
+            if (var is null)
+                entity.Has(out var);
+        }
+        public static bool Has<VarType>(this Entity entity, out VarType var)
+        {
+            if (!entity)
+            {
+                var = default;
+                return false;
+            }
+            return entity._ref.Has(out var);
+        }
+        public static bool Has<T1, T2>(this Entity entity, out T1 var1, out T2 var2)
+        {
+            if (!entity)
+            {
+                var1 = default;
+                var2 = default;
+                return false;
+            }
+            var result = entity.Has(out var1);
+            result &= entity.Has(out var2);
+            return result;
+        }
+        public static bool Has<T1, T2, T3>(this Entity entity, out T1 var1, out T2 var2, out T3 var3)
+        {
+            if (!entity)
+            {
+                var1 = default;
+                var2 = default;
+                var3 = default;
+                return false;
+            }
+            var result = entity.Has(out var1);
+            result &= entity.Has(out var2);
+            result &= entity.Has(out var3);
+            return result;
+        }
+        public static bool Has<T1, T2, T3, T4>(
+            this Entity entity,
+            out T1 var1, out T2 var2, out T3 var3, out T4 var4)
+        {
+            if (!entity)
+            {
+                var1 = default;
+                var2 = default;
+                var3 = default;
+                var4 = default;
+                return false;
+            }
+            var result = entity.Has(out var1);
+            result &= entity.Has(out var2);
+            result &= entity.Has(out var3);
+            result &= entity.Has(out var4);
+            return result;
+        }
+        public static bool Has<T1, T2, T3, T4, T5>(
+            this Entity entity,
+            out T1 var1, out T2 var2, out T3 var3, out T4 var4, out T5 var5)
+        {
+            if (!entity)
+            {
+                var1 = default;
+                var2 = default;
+                var3 = default;
+                var4 = default;
+                var5 = default;
+                return false;
+            }
+            var result = entity.Has(out var1);
+            result &= entity.Has(out var2);
+            result &= entity.Has(out var3);
+            result &= entity.Has(out var4);
+            result &= entity.Has(out var5);
+            return result;
+        }
 
-		public static void DespawnAndClear(this IList<Entity> entities)
-		{
-			foreach (var entity in entities)
-				entity.Despawn();
-			entities.Clear();
-		}
-	}
+        public static bool Has<T1, T2, T3, T4, T5, T6>(
+            this Entity entity,
+            out T1 var1, out T2 var2, out T3 var3, out T4 var4, out T5 var5, out T6 var6)
+        {
+            if (!entity)
+            {
+                var1 = default;
+                var2 = default;
+                var3 = default;
+                var4 = default;
+                var5 = default;
+                var6 = default;
+                return false;
+            }
+            var result = entity.Has(out var1);
+            result &= entity.Has(out var2);
+            result &= entity.Has(out var3);
+            result &= entity.Has(out var4);
+            result &= entity.Has(out var5);
+            result &= entity.Has(out var6);
+            return result;
+        }
+
+        public static void DespawnAndClear(this IList<Entity> entities)
+        {
+            foreach (var entity in entities)
+                entity.Despawn();
+            entities.Clear();
+        }
+    }
 }
