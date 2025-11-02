24e4669cf1fb93462058e099a59d510fcf35a646
Arseni <arseni.natapov@gmail.com>
2025-11-02 16:59:57 +0300

    Formatting

--- Base/Disposables/DisposableBag.cs
+++ Base/Disposables/DisposableBag.cs
@@ -2,35 +2,35 @@
 using System.Collections.Generic;
 namespace BB
 {
-	public sealed class DisposableAction : ProtectedPooledObject<DisposableAction>
-	{
-		Action _action;
-		public static DisposableAction GetPooled(Action action)
-		{
-			var result = GetPooledInternal();
-			result._action = action;
-			return result;
-		}
-		public override void Dispose()
-		{
-			_action?.Invoke();
-			base.Dispose();
-		}
-	}
-	public sealed class DisposableBag : PooledObject<DisposableBag>
-	{
-		readonly List<IDisposable> _disposables = new();
-		public void AddDisposable(IDisposable disposable)
-		{
-			if (disposable is not null)
-				_disposables.Add(disposable);
-		}
-		public override void Dispose()
-		{
-			foreach (var disposable in _disposables)
-				disposable.Dispose();
-			_disposables.Clear();
-			base.Dispose();
-		}
-	}
+    public sealed class DisposableAction : ProtectedPooledObject<DisposableAction>
+    {
+        Action _action;
+        public static DisposableAction GetPooled(Action action)
+        {
+            var result = GetPooledInternal();
+            result._action = action;
+            return result;
+        }
+        public override void Dispose()
+        {
+            _action?.Invoke();
+            base.Dispose();
+        }
+    }
+    public sealed class DisposableBag : PooledObject<DisposableBag>
+    {
+        readonly List<IDisposable> _disposables = new();
+        public void AddDisposable(IDisposable disposable)
+        {
+            if (disposable is not null)
+                _disposables.Add(disposable);
+        }
+        public override void Dispose()
+        {
+            foreach (var disposable in _disposables)
+                disposable.Dispose();
+            _disposables.Clear();
+            base.Dispose();
+        }
+    }
 }
--- Base/Pooling/ProtectedPooledObject.cs
+++ Base/Pooling/ProtectedPooledObject.cs
@@ -1,27 +1,44 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 namespace BB
 {
-	public abstract class ProtectedPooledObject<TSelf> : IPooledDisposable
-		where TSelf : ProtectedPooledObject<TSelf>, new()
-	{
-		static readonly List<TSelf> _pool = new();
-		public ulong Counter { get; private set; }
-		protected ProtectedPooledObject() { }
-		protected static TSelf GetPooledInternal()
-		{
+    public abstract class BasePooledObject : IPooledDisposable
+    {
+        public abstract ulong Counter { get; }
 
-			var result = _pool.Count == 0 ? new TSelf() : _pool.RemoveLast();
-			result.Counter = PooledDisposableUtils.GetNextCounter();
-			return result;
-		}
-		public virtual void Dispose()
-		{
-			if (Counter == 0)
-				return;
-			Counter = 0;
-			_pool.Add((TSelf)this);
-		}
-		public DisposableToken GetToken() => new(this);
-		public DisposableToken<TSelf> GetTypedToken() => new((TSelf)this, Counter);
-	}
+        public abstract void Dispose();
+        public static DisposableBag operator +(BasePooledObject l, IDisposable r)
+        {
+            var bag = DisposableBag.GetPooled();
+            bag.AddDisposable(l);
+            bag.AddDisposable(r);
+            return bag;
+        }
+        public static DisposableBag operator +(IDisposable l, BasePooledObject r)
+            => r + l;
+    }
+    public abstract class ProtectedPooledObject<TSelf> : BasePooledObject
+        where TSelf : ProtectedPooledObject<TSelf>, new()
+    {
+        static readonly List<TSelf> _pool = new();
+        ulong _counter;
+        public override ulong Counter => _counter;
+        protected ProtectedPooledObject() { }
+        protected static TSelf GetPooledInternal()
+        {
+
+            var result = _pool.Count == 0 ? new TSelf() : _pool.RemoveLast();
+            result._counter = PooledDisposableUtils.GetNextCounter();
+            return result;
+        }
+        public override void Dispose()
+        {
+            if (Counter == 0)
+                return;
+            _counter = 0;
+            _pool.Add((TSelf)this);
+        }
+        public DisposableToken GetToken() => new(this);
+        public DisposableToken<TSelf> GetTypedToken() => new((TSelf)this, Counter);
+    }
 }
