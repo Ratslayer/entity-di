f3e63b831edf6cb387840b3dcad58d1a2c335925
Arseni <arseni.natapov@gmail.com>
2025-11-14 20:11:58 +0300

    Update EntityVariable.cs

--- DI/Variables/EntityVariable.cs
+++ DI/Variables/EntityVariable.cs
@@ -3,25 +3,26 @@ using System.Threading;
 
 namespace BB.Di
 {
-	public abstract record EntityVariable<TSelf> : Variable<TSelf, Entity>
-		where TSelf : Variable<TSelf, Entity>
-	{
-		public bool Has<T>(out T comp)
-			=> Value.Has(out comp);
-		public bool Has<T1, T2>(out T1 c1, out T2 c2)
-			=> Value.Has(out c1, out c2);
-		public static implicit operator bool(EntityVariable<TSelf> b)
-			=> b.Value;
-		public void RaiseEvent<T>(T msg = default)
-			=> Value.Publish(msg);
-		public T Get<T>() => Value.Get<T>();
-		public void GetValue<T>(ref T cacheRef)
-			where T : class
-			=> Value.Get(ref cacheRef);
-		public async UniTask WaitForEvent<T>(CancellationToken ct)
-		{
-			if (Has(out IEvent<T> e))
-				await e.WaitForEvent(ct);
-		}
-	}
+    public abstract record EntityVariable<TSelf> : Variable<TSelf, Entity>
+        where TSelf : Variable<TSelf, Entity>
+    {
+        public bool Has<T>(out T comp)
+            => Value.Has(out comp);
+        public bool Has<T1, T2>(out T1 c1, out T2 c2)
+            => Value.Has(out c1, out c2);
+        public static implicit operator bool(EntityVariable<TSelf> b)
+            => b.Value;
+        public void RaiseEvent<T>(T msg = default)
+            => Value.Publish(msg);
+        public T Get<T>() => Value.Get<T>();
+        public T Require<T>() => Value.Require<T>();
+        public void GetValue<T>(ref T cacheRef)
+            where T : class
+            => Value.Get(ref cacheRef);
+        public async UniTask WaitForEvent<T>(CancellationToken ct)
+        {
+            if (Has(out IEvent<T> e))
+                await e.WaitForEvent(ct);
+        }
+    }
 }
