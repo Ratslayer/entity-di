f245237cb4b9142f7bce8d6115a0a47608e382cf
Arseni <arseni.natapov@gmail.com>
2025-11-16 19:40:19 +0300

    Update StringExtensions.cs

--- Base/Utils/StringExtensions.cs
+++ Base/Utils/StringExtensions.cs
@@ -2,7 +2,22 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
+public static class EnumExtensions
+{
+    public static bool FlagsIntersect<T>(this T? t1, T? t2)
+        where T : struct, Enum
+    {
+        if (t1 is null)
+            return false;
+        if (t2 is null)
+            return false;
 
+        var i1 = Convert.ToInt32(t1);
+        var i2 = Convert.ToInt32(t2);
+        var intersection = i1 & i2;
+        return intersection != 0;
+    }
+}
 public static class StringExtensions
 {
     static readonly StringBuilder _builder = new();
